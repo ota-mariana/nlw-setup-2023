d2194f0596576ebd37631a0f8d5da845517ddd51
Mariana <nanomarii10@gmail.com>
2023-01-19 05:39:40 -0300

    Implementando as rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,31 @@
 import { prisma } from './lib/prisma';
+import { z } from 'zod';
 import { FastifyInstance } from 'fastify';
+import dayjs from 'dayjs';
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/habits', async () => {
-    const habits = await prisma.habit.findMany()
-    return habits
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      habitWeekDays: z.array(z.number().min(0).max(6))
+    })
+
+    const { title, habitWeekDays } = createHabitBody.parse(request.body)
+
+    const today = dayjs().startOf('day').toDate()
+    
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        habitWeekDays: {
+          create: habitWeekDays.map(weekDay => {
+            return {
+              week_day: weekDay,
+            }
+          })
+        }
+      }
+    })
   })
 }
