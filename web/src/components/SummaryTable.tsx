b6b08c05928dd11f5177b6b92cc37e93caa1d6dc
Mariana <nanomarii10@gmail.com>
2023-01-22 15:40:05 -0300

    Correção da estilização dos hábitos e dias

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -21,16 +21,14 @@ function SummaryTable() {
         })}
       </div>
 
-      <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {summaryDates.map((date) => {
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map(date => {
           return <HabitDay key={ date.toString() } />
         })}
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
           return (
-            <div  key={ i } className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed">
-              aaa
-            </div>
+            <div  key={ i } className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
           )
         })}
 
